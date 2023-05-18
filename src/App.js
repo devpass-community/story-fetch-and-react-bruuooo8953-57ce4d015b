3b64067f87005b3ab5cd1b874a2910476efa08f5
Bruno Luis <bruno.dev07@gmail.com>
2023-05-18 10:20:24 -0300

    My fourth commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import { useState } from "react";
-import axios from "axios";
-import Container from "./components/Container";
-import Spinner from "./components/Spinner";
+import React, { useState } from 'react';
+import axios from 'axios';
+import Container from './components/Container';
+import Spinner from './components/Spinner';
 
 function App() {
   const [quote, setQuote] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
-
-
-  const handleClick = async (event) => {
-    setIsLoading(true)
-    event.preventDefault()
-    const responses = await axios.get('https://meowfacts.herokuapp.com/')
-    const phrase = responses.data.data
-    setQuote(phrase)
-    setIsLoading(false)
-    return(quote)
-  }
+  const handleClick = async () => {
+    setIsLoading(true);
+    
+    try {
+      const response = await axios.get('https://meowfacts.herokuapp.com/');
+      const phrase = response.data.data;
+      setQuote(phrase);
+    } catch (error) {
+      console.log('Error fetching quote:', error);
+      setQuote('Failed to fetch quote');
+    }
+    
+    setIsLoading(false);
+  };
 
   return (
     <Container>
-      <button data-testid="button" onClick={e => handleClick(e)}>
-        <span>get a fact</span>
+      <button data-testid="button" onClick={handleClick}>
+        <span>Get a fact</span>
       </button>
-      {isLoading || quote === '' ?
-        (<Spinner />) : (<span data-testid="quote">{quote}</span>)
-      }
+      {isLoading || quote === '' ? (
+        <Spinner />
+      ) : (
+        <span data-testid="quote">{quote}</span>
+      )}
     </Container>
   );
 }
